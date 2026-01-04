de3e948b7f6e70a43a204145aa3a834264f753be
aa-r-ju <aarju3447@gmail.com>
2026-01-04 22:27:24 +0545

    feat: to-uppercase

--- pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
+++ pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+let name = "ramkrishna";
+let upperName = name.toUpperCase();
+console.log(upperName);
+
+let lastName = "RAGHUBANSI";
+let lowerLastname = lastName.toLowerCase();
+console.log(lowerLastname);

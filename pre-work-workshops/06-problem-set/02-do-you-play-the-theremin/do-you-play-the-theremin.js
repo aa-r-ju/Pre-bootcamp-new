5ba00ee5b52bd537fbdfb9ae66e4eb4faa81e9ce
aa-r-ju <aarju3447@gmail.com>
2026-01-15 23:18:56 +0545

    feat: do-You-Play-The-Theremin

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(str) {
+  if (str[0] === "s" || str[0] === "S") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(doYouPlayTheTheremin("Griffin"));
+console.log(doYouPlayTheTheremin("Scott"));

e84590151df6c1f8b8e3bf4ed4637617f4b2a33b
aa-r-ju <aarju3447@gmail.com>
2026-01-15 22:26:15 +0545

    feat: truth-teller

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -1,8 +1,26 @@
-let boolean1 = true, boolean2 = true;
-// let boolean1 = true, boolean2 = false;
-// let boolean1 = false, boolean2 = true;
-// let boolean1 = false, boolean2 = false;
+// let boolean1 = true,
+//   boolean2 = true;
+// let boolean1 = true,
+//   boolean2 = false;
+let boolean1 = false,
+  boolean2 = true;
+// let betoolean1 = false,
+//   boolean2 = false;
 
 let result;
 
 // YOUR CODE BELOW
+if (boolean1 === true && boolean2 === true) {
+  result = "both";
+} else if (
+  boolean1 === true ||
+  boolean2 === false ||
+  boolean1 === false ||
+  boolean2 === true
+) {
+  result = "one";
+} else {
+  result = "none";
+}
+
+console.log(result);

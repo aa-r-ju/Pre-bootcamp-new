01620fbb49eff1b367deb622f95598b0f003d205
aa-r-ju <aarju3447@gmail.com>
2026-01-15 23:08:07 +0545

    feat: my-mnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function myMnemonic(...str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i]) {
+      result += str[i][0];
+    }
+  }
+
+  return result;
+}
+console.log(myMnemonic("Must", "Accept", "Sugar", "Happiness"));
+console.log(myMnemonic("We", "Eat", "Eggs"));

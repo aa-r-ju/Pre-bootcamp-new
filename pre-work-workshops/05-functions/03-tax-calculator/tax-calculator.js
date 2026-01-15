743b14a9bd7574037a9e83ec40e01524b9502533
aa-r-ju <aarju3447@gmail.com>
2026-01-15 22:42:32 +0545

    feat: tax-calculator

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function taxCalculator(price, state) {
+  if (state === "NY") {
+    return price + price * 0.04;
+  } else if (state === "NJ") {
+    return price + price * 0.06625;
+  }
+}
+console.log(taxCalculator(100, "NY"));
+console.log(taxCalculator(100, "NJ"));

ffd3488c1ff73ddf1ca8efac9c12901da8c85b87
aa-r-ju <aarju3447@gmail.com>
2026-01-15 22:34:22 +0545

    feat: default-greet

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,6 @@
 // YOUR CODE BELOW
+function defaultGreet(fname, lname = "Doe") {
+  return `Hi ${fname} ${lname}!`;
+}
+console.log(defaultGreet("Chaka", "Khan"));
+console.log(defaultGreet("John"));

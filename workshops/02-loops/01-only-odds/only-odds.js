40903ea0ffd69831074c8b09419c2a51a0431736
aa-r-ju <aarju3447@gmail.com>
2026-01-16 23:29:24 +0545

    feat: only-odds

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function onlyOdds(number) {
+  if (number < 1) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 !== 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(onlyOdds(6));

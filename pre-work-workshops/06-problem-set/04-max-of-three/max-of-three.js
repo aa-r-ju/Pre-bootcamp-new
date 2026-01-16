64b6a35c7dbba4213b2a8a1196cdda024b20d429
aa-r-ju <aarju3447@gmail.com>
2026-01-16 22:30:50 +0545

    feat: max of three

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function maxOfThree(...num) {
+  let max = num[0];
+  for (let i = 0; i < num.length; i++) {
+    if (max < num[i]) {
+      max = num[i];
+    }
+  }
+  return max;
+}
+console.log(maxOfThree(5, 4, 10));
+console.log(maxOfThree(7, 7, 4));

8356032fc64db29fc0cd67faca8e84f3c99e0cce
aa-r-ju <aarju3447@gmail.com>
2026-01-16 22:51:42 +0545

    feat: every-which-way

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function everyWhichWay(first, second, third) {
+  if (first + second === third) {
+    return "sum";
+  } else if (first - second === third) {
+    return "difference";
+  } else if (first * second === third) {
+    return "product";
+  } else if (first / second === third) {
+    return "fraction";
+  } else {
+    return null;
+  }
+}
+console.log(everyWhichWay(10, 20, 30));
+console.log(everyWhichWay(50, 20, 30));
+console.log(everyWhichWay(4, 4, 16));
+console.log(everyWhichWay(100, 10, 10));
+console.log(everyWhichWay(1, 1000, 50));

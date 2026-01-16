d71a78e52561674cf61e949fc6e69a2b0ab8b987
aa-r-ju <aarju3447@gmail.com>
2026-01-16 23:44:12 +0545

    feat: crazy-caps

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function crazyCaps(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (i % 2 === 0) {
+      result += str[i].toLowerCase();
+    } else {
+      result += str[i].toUpperCase();
+    }
+  }
+  return result;
+}
+console.log(crazyCaps("tej is amazing"));

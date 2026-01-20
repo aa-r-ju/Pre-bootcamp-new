65836b30c034cbf0324471097e3db6049e5a93fb
aa-r-ju <aarju3447@gmail.com>
2026-01-20 21:13:59 +0545

    feat: my-slice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx = 0, endIdx) {
+  let outPut = "";
+  if (endIdx === undefined) {
+    endIdx = originalString.length;
+  }
+  for (let i = startIdx; i < endIdx; i++) {
+    outPut += originalString[i];
+  }
+  return outPut;
+}
+console.log(mySlice("slice and dice"));
+console.log(mySlice("slice and dice", 2));
+console.log(mySlice("slice and dice", 2, 5));
+// let num = 10 - 4 = 6

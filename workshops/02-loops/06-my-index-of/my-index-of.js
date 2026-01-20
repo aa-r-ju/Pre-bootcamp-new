a273201f866eaf482134ed36d06cb08a8d6199a5
aa-r-ju <aarju3447@gmail.com>
2026-01-20 22:31:03 +0545

    feat: my-index-of

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function myIndexOf(source, searchValue, startIdx = 0) {
+  // loop through source starting from startIdx
+  for (let i = startIdx; i <= source.length - searchValue.length; i++) {
+    let found = true;
+
+    // check character by character
+    for (let j = 0; j < searchValue.length; j++) {
+      if (source[i + j] !== searchValue[j]) {
+        found = false;
+        break;
+      }
+    }
+
+    if (found) {
+      return i;
+    }
+  }
+
+  return -1;
+}

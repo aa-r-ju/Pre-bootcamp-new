e00678b6e4cab4a1e8153fb51b33e2aade1f034b
aa-r-ju <aarju3447@gmail.com>
2026-01-20 20:22:48 +0545

    feat: exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+  if (power === 0) {
+    return 1;
+  }
+  let result = base;
+  for (let i = 1; i < power; i++) {
+    result *= base;
+  }
+  return result;
+}
+// exponentiate(2, 2);
+console.log(exponentiate(3, 4));
+console.log(exponentiate(2, 2));
+console.log(exponentiate(5, 5));

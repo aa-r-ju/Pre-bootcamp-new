8b1fccfbba50af3f3e57489e9ad71944ddf462a1
aa-r-ju <aarju3447@gmail.com>
2026-01-23 00:01:03 +0545

    feat: zero-dark-thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(num) {
+  if (num === 0) {
+    return NaN;
+  }
+  let str = String(num);
+  let strValue = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "0") {
+      strValue += str[i];
+    }
+  }
+  return Number(strValue);
+}
+console.log(zeroDarkThirty(102302));
+console.log(zeroDarkThirty(606.203));

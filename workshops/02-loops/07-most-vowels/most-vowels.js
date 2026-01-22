88a85912609e9c4104934ec3013eb580f0d2ee07
aa-r-ju <aarju3447@gmail.com>
2026-01-22 19:30:28 +0545

    feat: most-vowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,35 @@
 // YOUR CODE BELOW
+function mostVowels(sentence) {
+  let maxVowels = 0;
+  let result = "";
+
+  let currentWord = "";
+  let currentCount = 0;
+
+  for (let i = 0; i < sentence.length; i++) {
+    let ch = sentence[i].toLowerCase();
+
+    if (ch !== " ") {
+      currentWord += sentence[i];
+
+      if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+        currentCount++;
+      }
+    } else {
+      if (currentCount > maxVowels) {
+        maxVowels = currentCount;
+        result = currentWord;
+      }
+      currentWord = "";
+      currentCount = 0;
+    }
+  }
+
+  if (currentCount > maxVowels) {
+    result = currentWord;
+  }
+
+  return result;
+}
+console.log(mostVowels("I am a keeper with some real rhythms"));
+console.log(mostVowels("try my gym"));

80a0c8f225cb5049c12d2015ed06b1a9e2023d9a
aa-r-ju <aarju3447@gmail.com>
2026-01-22 23:30:21 +0545

    feat: only-one

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function onlyOne(para1, para2, para3) {
+  let count = 0;
+  if (para1) count++;
+  if (para2) count++;
+  if (para3) count++;
+
+  return count === 1;
+}
+console.log(onlyOne(false, false, true));
+console.log(onlyOne(0, 1, 2));

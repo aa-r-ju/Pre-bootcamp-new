536704997b65b49455fad3896fd64f6906857345
aa-r-ju <aarju3447@gmail.com>
2026-01-24 22:14:44 +0545

    feat: odd-couple

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function oddCouple(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      newArray.push(array[i]);
+      if (newArray.length === 2) {
+        return newArray;
+      }
+    }
+  }
+  return newArray;
+}
+console.log(oddCouple([1, 2, 3, 4, 5]));
+console.log(oddCouple(2, 4, 6, 8));
+console.log(oddCouple([10, 15, 20]));
+console.log(oddCouple([2, 4, 6, 8]));

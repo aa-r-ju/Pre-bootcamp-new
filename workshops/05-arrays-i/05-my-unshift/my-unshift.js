655af3a2748246e2a92570936bad85f2658ef936
aa-r-ju <aarju3447@gmail.com>
2026-01-25 23:12:35 +0545

    feat: my-unShift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myUnshift(array, value) {
+  let newArray = [];
+  newArray.push(value);
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+console.log(myUnshift([1, 2, 3], 0));

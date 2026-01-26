8df8815dd9f3535c8817385cb9e40efa30f83c41
aa-r-ju <aarju3447@gmail.com>
2026-01-26 23:11:14 +0545

    feat: zoo-inventrory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function zooInventory(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i][0];
+    let animalType = array[i][1][0];
+    let age = array[i][1][1];
+
+    let sentence = `${name} the ${animalType} is ${age}.`;
+    newArray.push(sentence);
+  }
+  return newArray;
+}
+
+let myZoo = [
+  ["King Kong", ["gorilla", 42]],
+  ["Nemo", ["fish", 5]],
+  ["Punxsutawney Phil", ["groundhog", 11]],
+];
+
+console.log(zooInventory(myZoo));

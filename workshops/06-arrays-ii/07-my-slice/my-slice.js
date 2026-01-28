29fac853cfd05e9f033f3747e5183413672d4f4f
aa-r-ju <aarju3447@gmail.com>
2026-01-28 23:03:43 +0545

    feat: my-slice

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,41 @@
 // YOUR CODE BELOW
+function mySlice(originalArray, startIdx, endIdx) {
+  let result = [];
+  let length = originalArray.length;
+
+  // handle undefined startIdx
+  if (startIdx === undefined) {
+    startIdx = 0;
+  }
+
+  // handle undefined endIdx
+  if (endIdx === undefined) {
+    endIdx = length;
+  }
+
+  // handle negative startIdx
+  if (startIdx < 0) {
+    startIdx = length + startIdx;
+    if (startIdx < 0) startIdx = 0;
+  }
+
+  // handle negative endIdx
+  if (endIdx < 0) {
+    endIdx = length + endIdx;
+    if (endIdx < 0) endIdx = 0;
+  }
+
+  // main loop
+  for (let i = startIdx; i < endIdx && i < length; i++) {
+    result.push(originalArray[i]);
+  }
+
+  return result;
+}
+
+console.log(mySlice([1, 2, 3], 1, 2));
+console.log(mySlice([1, 2, 3], 1));
+console.log(mySlice([1, 2, 3]));
+console.log(mySlice([1, 2, 3], -1));
+console.log(mySlice([1, 2, 3], -2));
+console.log(mySlice([1, 2, 3], 0, -1));
